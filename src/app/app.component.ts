c37f8dea1283ed1a55acae03f8639f2d3cb1117b
LakshayRohilla <rlakshay14@gmail.com>
2022-10-30 18:42:50 +0530

    ngOnChange code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,4 +24,7 @@ export class AppComponent {
       content: blueprintData.serverContent
     });
   }
+  onChangeFirst() {
+    this.serverElements[0].name = "Changed!";
+  }
 }
--- src/app/section5server-element/section5server-element.component.ts
+++ src/app/section5server-element/section5server-element.component.ts
@@ -1,16 +1,24 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-section5server-element',
   templateUrl: './section5server-element.component.html',
   styleUrls: ['./section5server-element.component.scss']
 })
-export class Section5serverElementComponent implements OnInit {
+export class Section5serverElementComponent implements OnInit, OnChanges {
 
   @Input("srvElement") element: {type: string, name: string, content: string} = {type:  "", name: "", content: "" };
-  constructor() { }
+  @Input() name: string = "";
+  constructor() {
+    console.log("Constructor called")
+  }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChange called');
+    console.log(changes);
+  }
   ngOnInit(): void {
+    console.log("ngOnInit called")
   }
 
 }
