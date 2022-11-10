048a0754525416eba2ede1f724a83d9979dcb004
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2022-11-10 14:03:38 +0530

    V95 Using render to build better directives

--- src/app/section-7/section-7.module.ts
+++ src/app/section-7/section-7.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Section7Component } from './section7/section7.component';
 import {BasicHighlightDirective} from "./basic-highligh/basic-highlight.directive";
+import {BetterHighlightDirective} from "./better-highlight/better-highlight.directive";
 
 
 
 @NgModule({
   declarations: [
     Section7Component,
-    BasicHighlightDirective
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   exports: [
     Section7Component
