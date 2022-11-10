1ec04b80a614bc6c3aea5a7f604bafe74e9e126f
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2022-11-10 11:26:46 +0530

    v94 creating basic attribute directives

--- src/app/section-7/section-7.module.ts
+++ src/app/section-7/section-7.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Section7Component } from './section7/section7.component';
+import {BasicHighlightDirective} from "./basic-highligh/basic-highlight.directive";
 
 
 
 @NgModule({
   declarations: [
-    Section7Component
+    Section7Component,
+    BasicHighlightDirective
   ],
   exports: [
     Section7Component
