f1829b50a934ae63bac5fb2c36f651677753ace6
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2022-11-10 09:36:16 +0530

    V92 ngFor and ngIf recap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import {NgrxCounterOutputComponent} from "./ngrx/with-ngrx/ngrx-counter-output/n
 import {NgrxCounterComponent} from "./ngrx/with-ngrx/ngrx-counter/ngrx-counter.component";
 import {StoreModule} from "@ngrx/store";
 import {counterReducer} from "./ngrx/with-ngrx/state/counter.reducer";
+import {Section7Module} from "./section-7/section-7.module";
 
 @NgModule({
   declarations: [
@@ -37,8 +38,9 @@ import {counterReducer} from "./ngrx/with-ngrx/state/counter.reducer";
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    StoreModule.forRoot({counterStore: counterReducer})
+    StoreModule.forRoot({counterStore: counterReducer}),
     // here other than "counter" you can give any name and you would be using it inside the component.
+    Section7Module
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/section-7/section-7.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Section7Component } from './section7/section7.component';
+
+
+
+@NgModule({
+  declarations: [
+    Section7Component
+  ],
+  exports: [
+    Section7Component
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class Section7Module { }
